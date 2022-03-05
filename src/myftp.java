381ea55befcb55d9ce77bf9b5dad8239a205b388
shantisa <sns52193@uga.edu>
2022-03-05 14:08:28 -0500

    Worked on implementing terminate command

--- src/myftp.java
+++ src/myftp.java
@@ -5,16 +5,21 @@ import java.util.*;
 public class myftp {
     //initialize socket
     Socket socket = null;
+    Socket tsocket = null;
 
     DataInputStream inputStream = null;
     DataOutputStream outputStream = null;
+    DataInputStream tinput = null;
+    DataOutputStream toutput = null;
     FileInputStream fileInput = null;
     FileOutputStream fileOutput = null;
+    List<Integer> terminatedCommands = new ArrayList<>();
+    Boolean nSocketInUse = false;
 
     public static void main(String[] args) {
         try {
             myftp clientSocket = new myftp();
-            clientSocket.connection(args[0], args[1]);
+            clientSocket.connection(args[0], args[1], args[2]);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -22,104 +27,214 @@ public class myftp {
     }
 
     // connection is a function that establishes a connection with the server
-    public void connection(String ip, String port) throws IOException {
+    public void connection(String ip, String port, String tport) throws IOException {
         try {
             Scanner scanner = new Scanner(System.in);
 
             // establish connection with server
             socket = new Socket(ip, Integer.parseInt(port));
+            tsocket = new Socket(ip, Integer.parseInt(tport));
 
             // obtain input and out streams
             inputStream = new DataInputStream(socket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
+            tinput = new DataInputStream(tsocket.getInputStream());
+            toutput = new DataOutputStream(tsocket.getOutputStream());
 
             while (true) {
                 System.out.print("mytftp> ");
                 String input = scanner.nextLine();
                 String[] command = input.trim().split(" ");
 
-                String filename = "";
-                int read;
-                File file;
                 byte[] data;
+
+                if (command[0].equals("terminate")) {
+                    toutput.writeUTF(command[0]);
+                    int id = Integer.parseInt(command[1]);
+                    toutput.writeInt(id);
+                    boolean success = tinput.readBoolean();
+                    if (success) {
+                        synchronized (terminatedCommands) {
+                            terminatedCommands.add(id);
+                            inputStream.reset();
+                        }
+                    }
+                    String result = tinput.readUTF();
+                    System.out.println(result);
+                    continue;
+                }
+
+                boolean busy;
+                synchronized (nSocketInUse) {
+                    busy = nSocketInUse;
+                }
+                if (busy) {
+                    System.out.println("normal port is busy, you can use terminate or wait for upload/download to finish");
+                    continue;
+                }
 
                 outputStream.writeUTF(command[0]);
 
                 //implement commands
-                if(command[0].equals("get")){
-                    filename = command[1];
-                    outputStream.writeUTF(filename);
-                    long size = inputStream.readLong();
+                if (command[0].equals("get")) {
+                    String filename = command[1];
+                    boolean concurrent = command.length > 2 && command[2].equals("&");
+                    if (concurrent) {
+                        outputStream.writeUTF(filename + " &");
+                    } else {
+                        outputStream.writeUTF(filename);
+                    }
+                    final long finalSize = inputStream.readLong();
 
-                    if(size > 0){
-                        data = new byte[1024*100];
-                        File parent = new File(filename).getParentFile();
-                        if(parent!=null){
+                    if (finalSize > 0) {
+                        data = new byte[1000];
+                        File file = new File(filename);
+                        File parent = file.getParentFile();
+                        if (parent != null) {
                             parent.mkdirs();
                         }
-                        fileOutput = new FileOutputStream(filename);
+                        fileOutput = new FileOutputStream(file);
+                        if (concurrent) {
+                            int commandId = inputStream.readInt();
+                            System.out.println("command is running with id : " + commandId);
+                            final String fileId = file.getAbsolutePath();
 
-                        while (size > 0 && (read = inputStream.read(data, 0, (int)Math.min(data.length, size))) != -1) {
-                            fileOutput.write(data,0,read);
-                            size -= read;
+                            new Thread(() -> {
+                                synchronized (nSocketInUse) {
+                                    nSocketInUse = true;
+                                }
+                                try {
+                                    int read;
+                                    long size = finalSize;
+                                    boolean terminated = false;
+                                    while (size > 0 && (read = inputStream.read(data, 0, (int) Math.min(data.length, size))) != -1
+                                            && !terminated) {
+                                        synchronized (terminatedCommands) {
+                                            terminated = terminatedCommands.contains(commandId);
+                                        }
+                                        fileOutput.write(data, 0, read);
+                                        size -= read;
+                                    }
+                                    fileOutput.close();
+
+                                    if (terminated) {
+                                        file.delete();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    synchronized (nSocketInUse) {
+                                        nSocketInUse = false;
+                                    }
+                                    try {
+                                        inputStream.reset();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }).start();
+                        } else {
+                            int read;
+                            long size = finalSize;
+                            while (size > 0 && (read = inputStream.read(data, 0, (int) Math.min(data.length, size))) != -1) {
+                                fileOutput.write(data, 0, read);
+                                size -= read;
+                            }
+                            fileOutput.close();
                         }
-
-                        fileOutput.close();
-                    }else{
+                    } else {
                         System.out.println("Did not receive file from server");
                     }
-                } else if(command[0].equals("put")){
-                    filename = command[1];
+                } else if (command[0].equals("put")) {
+                    String filename = command[1];
+                    File file = new File(filename);
+                    boolean concurrent = command.length > 2 && command[2].equals("&");
+                    if (concurrent) {
+                        outputStream.writeUTF(file.getName() + " &");
+                    } else {
+                        outputStream.writeUTF(file.getName());
+                    }
 
-                    file = new File(filename);
-                    outputStream.writeUTF(file.getName());
-
-                    if(file.isFile()){
+                    if (file.isFile()) {
                         outputStream.writeLong(file.length());
                         fileInput = new FileInputStream(file);
-                        data = new byte[1024*100];
+                        data = new byte[1000];
+                        if (concurrent) {
+                            int commandId = inputStream.readInt();
+                            System.out.println("command is running with id : " + commandId);
+                            new Thread(() -> {
+                                synchronized (nSocketInUse) {
+                                    nSocketInUse = true;
+                                }
+                                try {
+                                    int read;
+                                    boolean terminated = false;
+                                    while ((read = fileInput.read(data)) != -1 && !terminated) {
+                                        synchronized (terminatedCommands) {
+                                            terminated = terminatedCommands.contains(commandId);
+                                        }
+                                        if(!terminated){
+                                            outputStream.write(data, 0, read);
+                                            outputStream.flush();
+                                        }
+                                    }
+                                    fileInput.close();
 
-                        while ((read=fileInput.read(data))!=-1){
-                            outputStream.write(data,0,read);
-                            outputStream.flush();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    synchronized (nSocketInUse) {
+                                        nSocketInUse = false;
+                                    }
+                                }
+                            }).start();
+                        } else {
+                            int read;
+                            while ((read = fileInput.read(data)) != -1) {
+                                outputStream.write(data, 0, read);
+                                outputStream.flush();
+                            }
+                            fileInput.close();
                         }
-                        fileInput.close();
-                    } else{
+                    } else {
                         System.out.println("File Not Found");
                         outputStream.writeLong(0); //No file
                     }
-                } else if(command[0].equals("delete")){
+                } else if (command[0].equals("delete")) {
                     outputStream.writeUTF(command[1]);
                     System.out.println(inputStream.readUTF());
-                }  else if(command[0].equals("ls")){
+                } else if (command[0].equals("ls")) {
                     String lsFiles = inputStream.readUTF();
                     System.out.println(lsFiles);
 
-                }  else if(command[0].equals("cd")){
+                } else if (command[0].equals("cd")) {
                     outputStream.writeUTF(command[1]);
                     String result = inputStream.readUTF();
                     System.out.println(result);
 
-                }  else if(command[0].equals("mkdir")){
+                } else if (command[0].equals("mkdir")) {
                     outputStream.writeUTF(command[1]);
                     System.out.println(inputStream.readUTF());
 
-                }  else if(command[0].equals("pwd")){
+                } else if (command[0].equals("pwd")) {
                     String pwd = inputStream.readUTF();
                     System.out.println(pwd);
-                }  else if(command[0].equals("quit")){
+                } else if (command[0].equals("quit")) {
                     outputStream.writeUTF(input);
                     break;
                 }
-
             }
-                socket.close();
-                inputStream.close();
-                outputStream.close();
-            } catch(Exception e){
-                System.out.println("Can't establish connection to Server");
-                e.printStackTrace();
-            }
-
+        } catch (Exception e) {
+            System.out.println("Can't establish connection to Server");
+            e.printStackTrace();
+        } finally {
+            socket.close();
+            inputStream.close();
+            outputStream.close();
+            tsocket.close();
+            tinput.close();
+            toutput.close();
         }
+
     }
+}
--- src/myftpserver.java
+++ src/myftpserver.java
@@ -3,42 +3,58 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class myftpserver {
-    //initialize socket
+    //initialize socket n-port
     Socket socket = null;
+    //initialize socket t-port
+    Socket tsocket = null;
+
     ServerSocket server = null;
+    ServerSocket tserver = null;
+    final List<String> filesInUse = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
             myftpserver serverSocket = new myftpserver();
-            serverSocket.connection(args[0]);
+            serverSocket.connection(args[0], args[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // connection is a function that establishes a connection with the client
-    public void connection(String port) throws IOException {
+    public void connection(String nport, String tport) throws IOException {
         try {
-            //server is listening
-            server = new ServerSocket(Integer.parseInt(port));
+            //server is listening on n-port
+            server = new ServerSocket(Integer.parseInt(nport));
+
+            //server is listening on t-port
+            tserver = new ServerSocket(Integer.parseInt(tport));
 
             System.out.print("Server is Running... ");
 
             //run infinite loop to get the client requests
             while (true) {
-                //receive incoming client request and establish connection
+                //receive incoming client request and establish connection for n-port
                 socket = server.accept();
 
+                //receive incoming client request and establish connection for t-port
+                tsocket = tserver.accept();
+
                 //create a new thread
-                ClientThread thread = new ClientThread(socket);
+                ClientThread thread = new ClientThread(socket, tsocket, filesInUse);
 
                 thread.start();
             }
 
         } catch (Exception e) {
             server.close();
+            tserver.close();
             e.printStackTrace();
 
         }
@@ -47,72 +63,239 @@ public class myftpserver {
 
 class ClientThread extends Thread {
     Socket socket = null;
+    Socket tsocket = null;
     DataInputStream inputStream = null;
+    DataInputStream tinput = null;
     DataOutputStream outputStream = null;
+    DataOutputStream toutput = null;
     FileInputStream fileInput = null;
     FileOutputStream fileOutput = null;
+    final List<Boolean> commandStatus = new ArrayList<>();
+    List<String> filesInUse;
 
     Path path = Paths.get("").toAbsolutePath();
 
     //Constructor
-    public ClientThread(Socket socket) {
+    public ClientThread(Socket socket, Socket tsocket,List<String> filesInUse) {
         try {
             this.socket = socket;
+            this.tsocket = tsocket;
+            this.filesInUse = filesInUse;
             inputStream = new DataInputStream(socket.getInputStream());
+            tinput = new DataInputStream(tsocket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
+            toutput = new DataOutputStream(tsocket.getOutputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    private void runTerminator(){
+        new Thread(()->{
+            try {
+                while (true) {
+                    String command = tinput.readUTF();
+                    if(command.equals("terminate")){
+                        int commandID = tinput.readInt();
+                        if(commandStatus.size() > commandID){
+                            boolean done;
+                            synchronized(commandStatus){
+                                done = !commandStatus.get(commandID);
+                                if(!done){
+                                    commandStatus.set(commandID, false);
+                                }
+                            }
+
+
+                            if(done){
+                                toutput.writeBoolean(!done);
+                                toutput.writeUTF("command already finished or terminated");
+                            }else{
+
+                            }
+                        }
+                        else{
+                            toutput.writeBoolean(false);
+                            toutput.writeUTF("command with this id doesn't exist");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+
     public void run() {
         try {
+            runTerminator();
             while (true) {
                 String command = inputStream.readUTF();
-                String filename = "";
                 File file;
 
                 byte[] data;
-                int read;
 
                 //testing purposes - server prints out the client command that was sent
                 //System.out.println(command);
 
                 //implement commands
                 if (command.equals("get")) {
-                    filename = inputStream.readUTF();
+                    String[] input = inputStream.readUTF().split(" ");
+                    String filename = input[0];
+                    boolean concurrent = input.length > 1 && input[1].equals("&");
                     file = path.resolve(filename).toFile();
 
                     if(file.isFile()){
                         outputStream.writeLong(file.length());
                         fileInput = new FileInputStream(file);
-                        data = new byte[1024*100];
+                        data = new byte[1000];
+                        if(concurrent){
+                            int commandId = -1;
+                            synchronized(commandStatus){
+                                commandId = commandStatus.size();
+                                commandStatus.add(true);
+                            }
+                            outputStream.writeInt(commandId);
+                            final int finalCommandId = commandId;
+                            try {
+                                int read;
+                                boolean terminated = false;
+                                while (!terminated &&(read=fileInput.read(data))!=-1){
+                                    synchronized(commandStatus){
+                                        terminated = !commandStatus.get(finalCommandId);
+                                    }
+                                    outputStream.write(data,0,read);
+                                    outputStream.flush();
+                                }
+                                fileInput.close();
 
-                        while ((read=fileInput.read(data))!=-1){
-                            outputStream.write(data,0,read);
-                            outputStream.flush();
+                                if(terminated){
+                                    toutput.writeBoolean(true);
+                                    toutput.writeUTF("command terminated");
+                                }
+
+                                synchronized(commandStatus){
+                                    commandStatus.set(commandId,false);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }else{
+                            int read;
+                            while ((read=fileInput.read(data))!=-1){
+                                outputStream.write(data,0,read);
+                                outputStream.flush();
+                            }
+                            fileInput.close();
                         }
-                        fileInput.close();
                     }else{
                         outputStream.writeLong(0); //No file
                     }
 
+
                 } else if (command.equals("put")) {
 
-                    filename = inputStream.readUTF();
-                    long size = inputStream.readLong();
+                    String[] input = inputStream.readUTF().split(" ");
+                    String filename = input[0];
+                    boolean concurrent = input.length > 1 && input[1].equals("&");
+                    final long finalSize = inputStream.readLong();
 
-                    if(size > 0) {
-                        data = new byte[1024 * 100];
-                        fileOutput = new FileOutputStream(path.resolve(filename).toFile());
+                    if(finalSize > 0) {
+                        data = new byte[1000];
+                        file = path.resolve(filename).toFile();
+                        fileOutput = new FileOutputStream(file);
+                        final String fileId = file.getAbsolutePath();
+                        if(concurrent){
+                            int commandId = -1;
+                            synchronized(commandStatus){
+                                commandId = commandStatus.size();
+                                commandStatus.add(true);
+                            }
+                            outputStream.writeInt(commandId);
+                            final int finalCommandId = commandId;
 
-                        while (size > 0 && (read = inputStream.read(data, 0, (int) Math.min(data.length, size))) != -1) {
-                            fileOutput.write(data, 0, read);
-                            size -= read;
+                            try {
+                                boolean inUse;
+                                do{
+                                    synchronized(filesInUse){
+                                        if(!filesInUse.contains(fileId)){
+                                            filesInUse.add(fileId);
+                                            inUse = false;
+                                        }
+                                        else{
+                                            inUse = true;
+                                        }
+                                    }
+                                    if(inUse){
+                                        filesInUse.wait();
+                                    }
+                                }while(inUse);
+
+                                int read;
+                                long size = finalSize;
+                                boolean terminated = false;
+                                while (size > 0 && !terminated &&
+                                        (read = inputStream.read(data, 0, (int) Math.min(data.length, size))) != -1) {
+                                    synchronized(commandStatus){
+                                        terminated = !commandStatus.get(finalCommandId);
+                                    }
+                                    fileOutput.write(data, 0, read);
+                                    size -= read;
+                                }
+                                fileOutput.close();
+
+                                if(terminated){
+                                    file.delete();
+                                    toutput.writeBoolean(true);
+                                    toutput.writeUTF("command terminated");
+                                }
+
+                                synchronized(filesInUse){
+                                    int nIndex = filesInUse.indexOf(fileId);
+                                    if(nIndex < 0){
+                                        throw new Exception("File in use list is invalid");
+                                    }
+                                    filesInUse.remove(nIndex);
+                                    filesInUse.notifyAll();
+                                }
+
+                                synchronized(commandStatus){
+                                    commandStatus.set(commandId,false);
+                                }
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                file.delete();
+                            }finally{
+                                inputStream.reset();
+                            }
+                        }else{
+                            boolean inUse;
+                            do{
+                                synchronized(filesInUse){
+                                    if(!filesInUse.contains(fileId)){
+                                        filesInUse.add(fileId);
+                                        inUse = false;
+                                    }
+                                    else{
+                                        inUse = true;
+                                    }
+                                }
+                                if(inUse){
+                                    filesInUse.wait();
+                                }
+                            }while(inUse);
+                            int read;
+                            long size = finalSize;
+                            while (size > 0 && (read = inputStream.read(data, 0, (int) Math.min(data.length, size))) != -1) {
+                                fileOutput.write(data, 0, read);
+                                size -= read;
+                            }
+                            fileOutput.close();
                         }
 
-                        fileOutput.close();
                     }
                 } else if (command.equals("delete")) {
                     String del = inputStream.readUTF();
@@ -163,6 +346,8 @@ class ClientThread extends Thread {
             }
             inputStream.close();
             outputStream.close();
+            tinput.close();
+            toutput.close();
 
         } catch (Exception e) {
             e.printStackTrace();
